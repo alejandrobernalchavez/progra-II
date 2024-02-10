91e5c4ac50fbdbe679df024649744183fc2895b3
Gerson Daniel Guerrero Castillo <USSS037423@ugb.edu.sv>
2024-02-10 11:38:02 -0600

    unidad nueva

--- calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -38,13 +38,32 @@ public class MainActivity extends AppCompatActivity {
                 spn= findViewById(R.id.spnLongitudDe);
                 int de =spn.getSelectedItemPosition();
 
-                spn= findViewById(R.id.spnLongitudDe);
+                spn= findViewById(R.id.spnLongitudA);
                 int a =spn.getSelectedItemPosition();
 
                 tempVal = findViewById(R.id.txtLongitudCantidad);
                 double cantidad = Double.parseDouble(tempVal.getText().toString());
 
                 Double resp= objconversor.convertir(0,de,a,cantidad);
+                Toast.makeText(getApplicationContext(),"Respuesta: "+
+                        resp, Toast.LENGTH_LONG).show();
+
+            }
+        });
+        btn = findViewById(R.id.btnAlmacenamientoConvertir);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spn= findViewById(R.id.spnAlmacenamientoDe);
+                int de =spn.getSelectedItemPosition();
+
+                spn= findViewById(R.id.spnAlmacenamientoA);
+                int a =spn.getSelectedItemPosition();
+
+                tempVal = findViewById(R.id.txtAlmacenamientoCantidad);
+                double cantidad = Double.parseDouble(tempVal.getText().toString());
+
+                Double resp= 1/objconversor.convertir(1,de,a,cantidad);
                 Toast.makeText(getApplicationContext(),"Respuesta: "+
                         resp, Toast.LENGTH_LONG).show();
 
@@ -57,10 +76,12 @@ public class MainActivity extends AppCompatActivity {
                 {1,100,39.3701,3.28084,1.193,1.0936133333333,0.001,0.000621371,0.001,0.000001,0.000000001
                 },
         //Almacenamiento
-        {1,8,1000*8,Math.pow(1,2)*8,Math.pow(1,3)*8,Math.pow(1,4)*8,Math.pow(1,5)*8,Math.pow(1,6)*8,Math.pow(1,7)*8,};
+        {1,8,1000*8,Math.pow(1,2)*8,Math.pow(1,3)*8,Math.pow(1,4)*8,Math.pow(1,5)*8,Math.pow(1,6)*8,Math.pow(1,7)*8,
+                1024*8,Math.pow(1024,2)*8,Math.pow(1024,3)*8,Math.pow(1024,4)*8,Math.pow(1024,5)*8,Math.pow(1024,6)*8,Math.pow(1024,7)*8,}
+        };
 
-        public double convertir (int opcion,int de,int a,double cantidad){
-            return  Valores [opcion][a]/Valores[opcion][de]*cantidad;
+        public double convertir(int opcion,int de,int a,double cantidad){
+            return  Valores[opcion][a]/Valores[opcion][de]*cantidad;
         }
         }
     }
