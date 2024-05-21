0da079c8fbd1eae97c5d85ea211ef640dcf16eee
leslyyuvicela <yuvicelaguzman14727@gmail.com>
2024-05-21 12:26:26 -0600

    chats amigos

---
+++ calculadora/app/src/main/java/com/example/calculadora/lista_amigos.java
@@ -0,0 +1,14 @@
+package com.example.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lista_amigos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_amigos);
+    }
+}
